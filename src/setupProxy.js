bfb620dbfd151a699ffd1bf907ff9acf6fc62b81
shawn_slxiao <shawn_slxiao@tutorabc.com.cn>
2020-07-06 17:45:24 +0800

    优化

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,12 @@
 const {createProxyMiddleware} = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/bk", {
-      target: "http://hydrogenstage.devops.tutorabc.com.cn/",
+    createProxyMiddleware("/", {
+      target: "",
       ws: true,
       changeOrigin: true,
       pathRewrite: {
       },
     }),
-    createProxyMiddleware("/ws", {
-      target: "ws://hydrogenstage.devops.tutorabc.com.cn",
-      ws: true,
-      changeOrigin: true,
-      pathRewrite: {
-      },
-    })
   );
 }
